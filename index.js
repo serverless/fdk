80945da10a3021751fc58b31d52366bfb29e1a67
Maciej Winnicki <mthenw@users.noreply.github.com>
2017-07-03 15:26:18 +0200

    make hander static method (#23)

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const AWS = require('aws-sdk')
 const FDK = require('./fdk')
 const Handler = require('./handler')
 
-exports = module.exports = handler => new Handler() // eslint-disable-line
-
 const fdk = new FDK(
   new AWS.Lambda({
     apiVersion: '2015-03-31',
@@ -12,3 +10,4 @@ const fdk = new FDK(
 
 exports.call = fdk.call
 exports.trigger = fdk.trigger
+exports.handler = handler => new Handler().handler(handler)
