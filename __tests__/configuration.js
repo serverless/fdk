c3fd5cd12d07aa4b5ee4161dbbe54890698fefdb
Nik Graf <nik@nikgraf.com>
2017-07-26 17:53:20 +0200

    start gateway in child process

--- __tests__/configuration.js
+++ __tests__/configuration.js
@@ -1,3 +1,18 @@
+const path = require('path')
+const spawn = require('child_process').spawn
+
+let eventGatewayProcess
+
+beforeAll(() => {
+  const gatewayPath = path.join(__dirname, 'event-gateway', 'darwin_amd64', 'event-gateway')
+  eventGatewayProcess = spawn(gatewayPath)
+})
+
+afterAll(() => {
+  eventGatewayProcess.stdin.pause()
+  eventGatewayProcess.kill()
+})
+
 test('placholder', () => {
   expect(true).toBeTruthy()
 })
