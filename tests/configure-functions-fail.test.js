89098167559f745b06d4c3b716572e00daf9f99b
Nik Graf <nik@nikgraf.com>
2017-08-24 13:28:21 +0200

    add better error messages for configure

---
+++ tests/configure-functions-fail.test.js
@@ -0,0 +1,58 @@
+const fdk = require('../lib/index')
+const eventGatewayProcesses = require('./event-gateway/processes')
+
+const config = {
+  functions: [
+    {
+      functionId: 'myFunctionOne',
+      provider: {
+        type: 'awslambda',
+        arn: 'https://xxxxxxxxxx.execute-api.us-east-1.amazonaws.com/dev/test',
+        region: 'us-east-1',
+      },
+    },
+    {
+      functionId: 'myFunctionTwo',
+      provider: {
+        type: 'http',
+      },
+    },
+    {
+      functionId: 'myFunctionThree',
+      provider: {
+        type: 'awslambda',
+        url: 'http://www.example.com',
+      },
+    },
+  ],
+  subscriptions: [{ functionId: 'myFunctionOne', event: 'pageVisited' }],
+}
+
+let eventGateway
+let eventGatewayProcessId
+
+beforeAll(() =>
+  eventGatewayProcesses
+    .spawn({
+      configPort: 4023,
+      apiPort: 4024,
+    })
+    .then(processInfo => {
+      eventGatewayProcessId = processInfo.id
+      eventGateway = fdk.eventGateway({
+        url: `http://localhost:${processInfo.apiPort}`,
+        configurationUrl: `http://localhost:${processInfo.configPort}`,
+      })
+    })
+)
+
+afterAll(() => {
+  eventGatewayProcesses.shutDown(eventGatewayProcessId)
+})
+
+test('should fail to create multiple misconfigured functions', () => {
+  expect.assertions(1)
+  return eventGateway.configure(config).catch(err => {
+    expect(err).toMatchSnapshot()
+  })
+})
