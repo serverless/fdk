ebe1b040de4b3dc3768c443df6dd8462753b6510
Nik Graf <nik@nikgraf.com>
2017-08-24 15:53:06 +0200

    verify that test fails

--- tests-browser/browser-test.js
+++ tests-browser/browser-test.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer')
 const path = require('path')
 
 const command = `
-const eventGateway = fdk.eventGateway({
+const eventGateway = jhkhkhk.eventGateway({
   url: 'http://localhost',
 })
 `
