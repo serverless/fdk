3ef94dacaf3e725a692de75af3451d916d9b4e25
Philipp Muens <raute1337@gmx.de>
2017-12-08 12:05:30 +0100

    Add functionality to auto-inject the authorization token

---
+++ tests/utils.headers.injectAuthorizationToken.test.js
@@ -0,0 +1,25 @@
+const headersUtils = require('../lib/utils/headers')
+
+afterEach(() => {
+  delete process.env.SERVERLESS_APPLICATION_TOKEN
+})
+
+test('should inject the Authorization header if serverless application token is present', () => {
+  process.env.SERVERLESS_APPLICATION_TOKEN = 'wasd-1234'
+  const headers = {
+    'Content-Type': 'application/json',
+  }
+  expect(headersUtils.injectAuthorizationToken(headers)).toEqual({
+    'Content-Type': 'application/json',
+    Authorization: 'bearer wasd-1234',
+  })
+})
+
+test('should return the same headers if serverless application token is NOT present', () => {
+  const headers = {
+    'Content-Type': 'application/json',
+  }
+  expect(headersUtils.injectAuthorizationToken(headers)).toEqual({
+    'Content-Type': 'application/json',
+  })
+})
